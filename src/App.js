e3af1a880fd0c08d5974e5c8cfc20ad1bdb8250a
DevManish-07 <sendtomanish0721@gmail.com>
2022-03-26 19:21:03 +0530

    Created initial components for sidebar, Header, Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import { Sidebar } from "./pagecomponents/sidebar/side_bar";
+import { Header } from "./pagecomponents/header/header";
+import { Footer } from "./pagecomponents/footer/footer";
 
 function App() {
   return (
     <div class="wrapper">
-      <div class="header"></div>
-      <div class="sidebar"></div>
-      <div class="content"></div>
-      <div class="footer"></div>
+      <div class="header">
+        <Header />
+      </div>
+      <div class="sidebar">
+        <Sidebar />
+      </div>
+      <div class="content">content</div>
+      <div class="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
