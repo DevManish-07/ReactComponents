5c492a64467919aae695b845acb4788e0d5043a2
DevManish-07 <sendtomanish0721@gmail.com>
2022-03-27 00:59:13 +0530

    Updated header

--- src/pagecomponents/header/header.js
+++ src/pagecomponents/header/header.js
@@ -1,5 +1,21 @@
+import "./header.css";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <strong>
+        <span className="main-title">React custom Components</span>
+      </strong>
+      <ul className="main-menu">
+        <li>Home</li>
+        <li>Docs</li>
+        <li>Examples</li>
+        <li>Icons</li>
+        <li>Themes</li>
+        <li>Blog</li>
+      </ul>
+    </header>
+  );
 };
 
 export { Header };
